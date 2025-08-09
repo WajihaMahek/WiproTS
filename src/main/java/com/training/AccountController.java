cb01181bd774b7b8a46efb28927fa6d83c218b34
srini <srinipassion27@gmail.com>
2025-08-09 10:19:42 +0530

    Spring boot project

---
+++ src/main/java/com/training/AccountController.java
@@ -0,0 +1,13 @@
+package com.training;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+    @GetMapping("/api/status")
+    public String getStatus() {
+        return "Telecom Service is up and running.";
+    }
+}
